517662552c6857ed11aa6e9d4401d95c74f588a7
QianDingwei <qiandingwei@xiaomi.com>
2019-07-26 19:32:46 +0800

    修改收藏页面改变主题

--- js/page/FavoritePage.js
+++ js/page/FavoritePage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
+import { connect } from 'react-redux'
+import actions from '../action/index.js'
 
-export default class FavoritePage extends React.Component {
+class FavoritePage extends React.Component {
   render() {
     const { navigation } = this.props
 
@@ -12,12 +14,7 @@ export default class FavoritePage extends React.Component {
         <Button
           title="改变主题色--绿色"
           onPress={() => {
-            navigation.setParams({
-              theme: {
-                tintColor: 'green',
-                updateTime: new Date().getTime()
-              }
-            })
+            this.props.onThemeChange('green')
           }}
         />
       </View>
@@ -36,3 +33,16 @@ const styles = StyleSheet.create({
     fontSize: 20
   }
 })
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onThemeChange: theme => dispatch(actions.onThemeChange(theme))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FavoritePage)
