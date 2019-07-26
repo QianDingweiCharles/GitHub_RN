b1743d128253f3c88651afdeb198aed2279805cd
QianDingwei <qiandingwei@xiaomi.com>
2019-07-26 20:11:12 +0800

    处理Android物理返回键

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import { BackHandler } from 'react-native'
+import { NavigationActions } from 'react-navigation'
+import { connect } from 'react-redux'
 import NavigationUtil from '../navigator/NavigationUtil'
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator'
 
-export default class HomePage extends React.Component {
+class HomePage extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  onBackPress = () => {
+    const { dispatch, nav } = this.props
+    if (nav.routes[1].index === 0) {
+      //如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+      return false
+    }
+    dispatch(NavigationActions.back())
+    return true
+  }
+
   render() {
     NavigationUtil.navigation = this.props.navigation
     return <DynamicTabNavigator />
   }
 }
+
+const mapStateToProps = state => ({
+  nav: state.nav
+})
+
+export default connect(mapStateToProps)(HomePage)
